2891a10b945a157ad33832a586cc3e85c74892bf
abhis <abhis@host.docker.internal>
2021-07-08 10:14:58 +0530

    first commit

---
+++ techmahindra/src/test/java/techmahindra/FirstSelenium.java
@@ -0,0 +1,21 @@
+package techmahindra;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstSelenium {
+	
+	@Test
+	public void loginToGoogle() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\abhis\\Documents\\Development\\ChromeDriver\\ChromeDriver.exe"); 
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+		driver.manage().timeouts().pageLoadTimeout(4, TimeUnit.SECONDS);
+		driver.quit();
+	}
+}
+
